36c42e19ce8191ff7f4d62902a330bcc6815a93c
rohit172 <dadwalrohit153@gmail.com>
2020-10-09 16:55:32 +0100

    init commit

---
+++ chmbac/home.php
@@ -0,0 +1,24 @@
+<style type="text/css">
+.home >  .pix_diapo , .da-thumbs{
+  width: 100%;
+}
+ .pix_diapo div  img{
+    width: 100%;
+    height: 100%;
+ }
+</style>
+
+<div class="home"> 
+    <div class="pix_diapo">
+
+    <div data-thumb="<?php echo web_root;?>image/image11.jpg">
+        <img src="<?php echo web_root;?>image1/image11.jpg">
+    </div>
+
+    <div data-thumb="<?php echo web_root;?>image/image22.jpg">
+        <img src="<?php echo web_root;?>image/image22.jpg"> 
+    </div>
+
+    </div>
+</div>
+
